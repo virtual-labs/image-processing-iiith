43611e06d9b3f61e8e4c3642dfe766b77db361c2
ENikhil <e_sai_nikhil@yahoo.com>
2019-03-28 03:02:40 +0530

    Reference tab uncommented

--- src/lab/point.php
+++ src/lab/point.php
@@ -504,8 +504,8 @@ $("button3","#zoom").button();
 			   </ul>
 
 </li>
-<!--<li><a href="references.php?exp=point" target="_self" >References</a>
-</li>-->
+<li><a href="references.php?exp=point" target="_self" >References</a>
+</li>
 <li><a href="summary.php" target="_blank" >Summary</a>
 </li>
 </ul>
